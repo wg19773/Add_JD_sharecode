f77b40963f081ac47a8614103af7d467d5e9c6bc
wg19773 <75625991+wg19773@users.noreply.github.com>
2021-01-18 12:38:36 +0800

    Update Add_code.py

--- Add_code.py
+++ Add_code.py
@@ -8,6 +8,8 @@ ncUrl = 'http://jd.turinglabs.net/api/v2/jd/farm/create/sharecode/' #农场
 mcUrl = 'http://jd.turinglabs.net/api/v2/jd/pet/create/sharecode/'  #萌宠
 ddUrl = 'http://jd.turinglabs.net/api/v2/jd/ddfactory/create/sharecode/' #东东工厂
 jxUrl = 'http://jd.turinglabs.net/api/v2/jd/jxfactory/create/sharecode/' #京喜工厂
+joyUrl = 'https://code.chiang.fun/api/v1/jd/jdcrazyjoy/create/sharecode/' #疯狂的Joy jdcrazyjoy
+
 
 #Defalt_cfdShareCode= ['Jxcfd_GroupId_143_1099534112113','AUWE56NTjs2BobyK6miEf','ATGcUnqSUyDUNCGE','A0pLfRgoZzDcKCg']#读取参数djj_sharecode为空，开始读取默认参数
 Defalt_zzShareCode= ['ZEw2Zeu1Zw', 'eU9YG5XBGKlGliyXjC5G', 'ZEl1beW2Y_wj8W8', '-ry-tUs7Z_4k8w', 'cVlmKrO7Z_RJojA']#读取参数djj_sharecode为空，开始读取默认参数
@@ -16,6 +18,7 @@ Defalt_mcShareCode= ['MTAxODc2NTEzMDAwMDAwMDAwNjIzMDM5Nw==','MTAxODc2NTEzNDAwMDA
 Defalt_zdShareCode= ['kgaroyrauafivcbp2cpznl3xoa','za6kg6vamtzyyelngvu4egas74','66nvo67oyxpydyknid7gnb5hl64zkefehrpk27q','kxg6vgmnlej6hotdtd4wokqs7m','bt6wkxd3txz7nvdqud4jynzof4']#读取参数djj_sharecode为空，开始读取默认参数
 Defalt_ddShareCode= ['P04z54XCjVWnYaS5nJfCWn_2n0','P04z54XCjVWnYaS5uyvgblfVH1Il5w','P04z54XCjVWnYaS5m9cZxeBrgIf9W22TeJwZg','P04z54XCjVWnYaS5nJaSmHx2XlKkLQ4','bt6wkxd3txz7nvdqud4jynzof4']#读取参数djj_sharecode为空，开始读取默认参数
 Defalt_jxShareCode= ['bKIstNuvE23fHdtW6HzzZg==','E_5JESV4tsmaifaCH98bSA==','AYRhSDoylQZ60BB8vIxFmA==','EoEhmxJM8mrye4W5lnlEOA==']#读取参数djj_sharecode为空，开始读取默认参数
+Defalt_joyShareCode= ['rFdQSg-fMoE=', 'CkZ8cwV57JL9Q4pf3AGYoA==', 'JayfVNVRmUO_He8bfCZV3w==', 'FOXSzvD-nhm7iQCQ91EeXQ==', 'tC3NXEXwLJs_TKGRDxw2WA==']#读取参数djj_sharecode为空，开始读取默认参数
 
 #学习与测试  by 2020.12
 def AddhelpCode(Url,Defalt_ShareCode):
@@ -44,3 +47,4 @@ AddhelpCode(ncUrl,Defalt_ncShareCode)
 AddhelpCode(zdUrl,Defalt_zdShareCode)
 AddhelpCode(ddUrl,Defalt_ddShareCode)
 AddhelpCode(jxUrl,Defalt_jxShareCode)
+AddhelpCode(joyUrl,Defalt_joyShareCode)
